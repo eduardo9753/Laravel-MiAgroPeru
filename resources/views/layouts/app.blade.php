cd16aac852fb5c33ce5ef6663db1bfbc9a9d7bea
Eduardo <picis_9716@hotmail.com>
2023-04-16 19:54:22 -0500

    Update Home

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 
     <title>MiAgroPeru - @yield('titulo')</title>
