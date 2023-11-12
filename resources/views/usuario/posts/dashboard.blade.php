024c866ae00048a8537ed430f4c68008baa2cbd9
Eduardo <picis_9716@hotmail.com>
2023-11-12 16:38:21 -0500

    Plataforma Miagroperu v.1.0

--- resources/views/usuario/posts/dashboard.blade.php
+++ resources/views/usuario/posts/dashboard.blade.php
@@ -32,7 +32,7 @@
                     {{-- SI PERFIL AUTENTICADO ES IGUAL AL AUTENTICADO ENTONCES EDITAMOS --}}
                     @auth
                         @if ($user->id === auth()->user()->id)
-                            <a class="btn btn-outline-warning btn-sm text-white" href="{{ route('perfil.index') }}" class=""><i class='bx bx-edit-alt bx-tada'></i></a>
+                            <a class="btn btn-warning btn-sm text-white" href="{{ route('perfil.index') }}" class=""><i class='bx bx-edit-alt bx-tada'></i></a>
                         @endif
                     @endauth
                 </div>
